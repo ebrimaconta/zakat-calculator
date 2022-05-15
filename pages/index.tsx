6f549ad640298eb274cfcb7b4c8e9c1f68ea9254
abdul t <abdul.tarofdar@xansium.com>
2022-05-15 23:59:03 +0100

    remove unused imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,6 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import Link from 'next/link';
-import Calculator from '../components/Calculator';
-
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
