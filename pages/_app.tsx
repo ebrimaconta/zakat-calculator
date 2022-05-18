27ec831d75050e686c553b63166f172d4ea6594b
abdul t <abdul.tarofdar@xansium.com>
2022-05-18 22:21:29 +0100

    Remove unwanted files

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import {Button} from '../components/button.styled'
 
 
 function MyApp({ Component, pageProps }: AppProps) {
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,6 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import {Button} from '../components/button.styled'
 import {Wrapper} from '../components/wrapper.styled'
 import { Title } from '../components/title.styled'
 const Home: NextPage = () => {
