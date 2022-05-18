76dfcb2dc816f783c73615f1da898c66dfcde6f8
abdul t <abdul.tarofdar@xansium.com>
2022-05-18 22:19:09 +0100

    Title and background using styled components

---
+++ components/title.styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Title = styled.h1`
+  margin: 0;
+  font-size: 4em;
+  text-align: center;
+  color: #FFD9C0;
+  padding-top: 5rem;
+`
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import {Button} from '../components/button.styled'
+
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
