a0b8f32fe1de55c204317323048b10edc5e30986
abdul t <abdul.tarofdar@xansium.com>
2022-07-17 22:01:02 +0100

    format file

--- components/Home/Calculator/Calculator.test.jsx
+++ components/Home/Calculator/Calculator.test.jsx
@@ -4,32 +4,29 @@ import { render, screen, waitFor, fireEvent } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 
 describe('Calculator form', () => {
-    it('Should render Calculator', () => {
-        render(<CalculatorForm />)
-        expect(screen.getByRole('heading')).toHaveTextContent('Enter amounts')
-    })
-              
-    it('rendering and submitting form', async () => {
-        const onSubmit = jest.fn()
-        render(<CalculatorForm onSubmit={onSubmit}/>)
-        const user = userEvent.setup()
+  it('Should render Calculator', () => {
+    render(<CalculatorForm />)
+    expect(screen.getByRole('heading')).toHaveTextContent('Enter amounts')
+  })
 
-    const goldSilver = document.querySelector('input[name="goldSilver"]');
-    const cash = document.querySelector('input[name="cash"]');
-    const buisnessAssets = document.querySelector('input[name="buisnessAssets"]');
-    const liabilities = document.querySelector('input[name="liabilities"]');
-    
+  it('rendering and submitting form', async () => {
+    const onSubmit = jest.fn()
+    render(<CalculatorForm onSubmit={onSubmit} />)
+    const user = userEvent.setup()
+
+    const goldSilver = document.querySelector('input[name="goldSilver"]')
+    const cash = document.querySelector('input[name="cash"]')
+    const buisnessAssets = document.querySelector('input[name="buisnessAssets"]')
+    const liabilities = document.querySelector('input[name="liabilities"]')
+
     await user.type(goldSilver, '100')
     await user.type(cash, '100')
     await user.type(buisnessAssets, '100')
     await user.type(liabilities, '100')
 
-     fireEvent.click(document.querySelector('button[type="submit"]'));
-        
-        const due = document.querySelector('p')
-        await waitFor(() =>
-            expect(due).toHaveTextContent('Zakat due: £ 5'))
-    
+    fireEvent.click(document.querySelector('button[type="submit"]'))
+
+    const due = document.querySelector('p')
+    await waitFor(() => expect(due).toHaveTextContent('Zakat due: £ 5'))
   })
-   
-    })
+})
