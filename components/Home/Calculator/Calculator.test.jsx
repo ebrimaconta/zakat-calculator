3b4d0141ad58aa774f09e698382872d9cfad35fe
abdul t <abdul.tarofdar@xansium.com>
2022-07-26 06:22:31 +0100

    change unit tests to use getbyPlaceHolder

--- components/Home/Calculator/Calculator.test.jsx
+++ components/Home/Calculator/Calculator.test.jsx
@@ -10,22 +10,17 @@ describe('Calculator form', () => {
   })
 
   it('rendering and submitting form', async () => {
-    render(<CalculatorForm/>)
+    render(<CalculatorForm />)
     const user = userEvent.setup()
 
-    const goldSilver = document.querySelector('input[name="goldSilver"]')
-    const cash = document.querySelector('input[name="cash"]')
-    const buisnessAssets = document.querySelector('input[name="buisnessAssets"]')
-    const liabilities = document.querySelector('input[name="liabilities"]')
+    await user.type(screen.getByPlaceholderText('Gold and Silver'), '100')
+    await user.type(screen.getByPlaceholderText('Cash'), '100')
+    await user.type(screen.getByPlaceholderText('Buisness assets'), '100')
+    await user.type(screen.getByPlaceholderText('Short term liabilities'), '50')
 
-    await user.type(goldSilver, '100')
-    await user.type(cash, '100')
-    await user.type(buisnessAssets, '100')
-    await user.type(liabilities, '100')
-
-    user.click(document.querySelector('button[type="submit"]'))
+    user.click(screen.getByText(/Submit/i))
 
     const due = document.querySelector('p')
-    await waitFor(() => expect(due).toHaveTextContent('Zakat due: £ 5'))
+    await waitFor(() => expect(due).toHaveTextContent('Zakat due: £ 6'))
   })
 })
--- components/Home/Calculator/CalculatorForm.js
+++ components/Home/Calculator/CalculatorForm.js
@@ -1,13 +1,11 @@
 import React, { useState } from 'react'
 import { Form, Div, Input, Due, SubmitButton, Error, H3 } from '../Calculator/CalculatorForm.styled'
-import { useFormik, Formik,} from 'formik'
+import { useFormik, Formik } from 'formik'
 import * as yup from 'yup'
-
-
 
 export const CalculatorForm = () => {
   let [dueAmount, calculateAmount] = useState('')
-  
+
   const validationSchema = yup.object().shape({
     goldSilver: yup.number().required('Please enter a amount'),
     cash: yup.number().required('Please enter a amount'),
@@ -24,60 +22,60 @@ export const CalculatorForm = () => {
     },
     validationSchema: validationSchema,
     onSubmit: (values) => {
-      const totalDue = Math.round((values.buisnessAssets + values.cash + values.goldSilver - values.liabilities) * 0.025)
+      const totalDue = Math.round(
+        (values.buisnessAssets + values.cash + values.goldSilver - values.liabilities) * 0.025
+      )
       calculateAmount(totalDue)
-    },
+    }
   })
   return (
     <Div>
       <H3>Enter amounts</H3>
       <Formik>
         <Form onSubmit={formik.handleSubmit}>
-            <label htmlFor='goldSilver'>Gold and Silver</label>
-            <Input
-              name='goldSilver'
-              type='number'
-              onChange={formik.handleChange}
-              value={formik.values.goldSilver}
-        />
-              {formik.touched.goldSilver && formik.errors.goldSilver ? (
-              <Error>{formik.errors.goldSilver}</Error>
-              ) : null}
-        
-            <label htmlFor='Cash'>Cash</label>
-            <Input
-              name='cash'
-              type='number'
-              onChange={formik.handleChange}
-          value={formik.values.cash}
-        />
-         {formik.touched.cash && formik.errors.cash ? (
-              <Error>{formik.errors.cash}</Error>
-              ) : null}
-            <label htmlFor='buisnessAssets'>Buisness assets</label>
-            <Input
-              name='buisnessAssets'
-              type='number'
-              onChange={formik.handleChange}
-              value={formik.values.buisnessAssets}
-        />
+          <label htmlFor="goldSilver">Gold and Silver</label>
+          <Input
+            name="goldSilver"
+            type="number"
+            placeholder="Gold and Silver"
+            onChange={formik.handleChange}
+            value={formik.values.goldSilver}
+          />
+          {formik.touched.goldSilver && formik.errors.goldSilver ? <Error>{formik.errors.goldSilver}</Error> : null}
+
+          <label htmlFor="Cash">Cash</label>
+          <Input
+            name="cash"
+            type="number"
+            placeholder="Cash"
+            onChange={formik.handleChange}
+            value={formik.values.cash}
+          />
+          {formik.touched.cash && formik.errors.cash ? <Error>{formik.errors.cash}</Error> : null}
+          <label htmlFor="buisnessAssets">Buisness assets</label>
+          <Input
+            name="buisnessAssets"
+            type="number"
+            placeholder="Buisness assets"
+            onChange={formik.handleChange}
+            value={formik.values.buisnessAssets}
+          />
           {formik.touched.buisnessAssets && formik.errors.buisnessAssets ? (
-              <Error>{formik.errors.buisnessAssets}</Error>
-              ) : null}
-            <label htmlFor='liabilities'>Short term liabilities</label>
-            <Input
-              name='liabilities'
-              type='number'
-              onChange={formik.handleChange}
-              value={formik.values.liabilities}
-        />
-         {formik.touched.liabilities && formik.errors.liabilities ? (
-              <Error>{formik.errors.liabilities}</Error>
-              ) : null}
-           <SubmitButton type='submit'>Submit</SubmitButton>
+            <Error>{formik.errors.buisnessAssets}</Error>
+          ) : null}
+          <label htmlFor="liabilities">Short term liabilities</label>
+          <Input
+            name="liabilities"
+            type="number"
+            placeholder="Short term liabilities"
+            onChange={formik.handleChange}
+            value={formik.values.liabilities}
+          />
+          {formik.touched.liabilities && formik.errors.liabilities ? <Error>{formik.errors.liabilities}</Error> : null}
+          <SubmitButton type="submit">Submit</SubmitButton>
         </Form>
-        </Formik>
-      <Due>Zakat due: £ { dueAmount }</Due>
+      </Formik>
+      <Due>Zakat due: £ {dueAmount}</Due>
     </Div>
   )
 }
